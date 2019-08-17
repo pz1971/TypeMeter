147e6ff5122f0f236bf5d613b6b03342ffaa02c7
Parvez Mia <parvez.pz1971@gmail.com>
2019-08-18 02:28:06 +0600

    project initiated and login page

---
+++ TypeMeter/src/typemeter/User.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package typemeter;
+
+/**
+ *
+ * @author Parvez
+ */
+public class User {
+    public String user_name, password;
+    
+    User(String user_name, String password){
+        this.user_name = user_name;
+        this.password = password;
+    }
+}
