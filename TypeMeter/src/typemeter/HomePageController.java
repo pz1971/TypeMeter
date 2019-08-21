9c3909ddbb3ae63b45a4cd5b5482a06647b07e80
Parvez Mia <parvez.pz1971@gmail.com>
2019-08-22 00:21:31 +0600

    typing visualization (color text and progress bar)

--- TypeMeter/src/typemeter/HomePageController.java
+++ TypeMeter/src/typemeter/HomePageController.java
@@ -7,9 +7,14 @@ package typemeter;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextArea;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
@@ -32,18 +37,90 @@ public class HomePageController implements Initializable {
     TextFlow typeScript ;
     
     @FXML
-    public void StartTypingButtonActon(ActionEvent event){
-        String script = (new ScriptGenerator()).generate();
-        text1.setText(script);
+    Button startTypingButton;
+    
+    @FXML
+    TextArea typeArea;
+    
+    @FXML
+    ProgressBar pb;
+    
+    String script;
+    int correct = 0, incorrect = 0;
+    
+    @FXML
+    public void startTypingButtonActon(ActionEvent event){
+        script = (new ScriptGenerator()).generate();    // generates a random script
+        text1.setText("");      // already typed correctly
+        text2.setText("");      // typed but not correct
+        text3.setText(script);  // remaining protion of the script not yet 'd been typed
+        
+        startTypingButton.setVisible(false);    // button disappears :D
+        correct = incorrect = 0;
+        pb.setProgress(0.0) ;       // 0% has been typed
+        typeArea.setText("");
+    }
+    
+    private void endOfScript(){ // when user finishes typing the current script
+        startTypingButton.setVisible(true);     // you can type again... :)
+        
+        script = "";
+        text1.setText("");
         text2.setText("");
         text3.setText("");
+        
+        pb.setProgress(1.0);
+        correct = incorrect = 0;
     }
+    
+    private void calculate(String typed){
+        correct = 0;
+        for(int i=0; i<typed.length(); i++){
+            if(script.charAt(i) == typed.charAt(i)){
+                correct++;
+            }
+            else
+                break;
+        }
+        
+        pb.setProgress(correct / (double)script.length()) ;
+        incorrect = typed.length() - correct;
+    }
+    
+    private void i_am_typing(){
+        String typed = typeArea.getText();
+        
+        if(typed.equals(script)){
+            endOfScript();
+            return;
+        }
+        else{
+            if(typed.length() > script.length()){
+                typeArea.setText(typed.substring(0, script.length())) ;     // can't type more than required... :)
+            }
+            else{
+                calculate(typed);
+                
+                text1.setText(script.substring(0, correct)) ;
+                text2.setText(script.substring(correct, correct + incorrect));
+                text3.setText(script.substring(correct + incorrect, script.length()));
+            }
+        }
+    }
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         text1.setFill(Color.GREEN);
         text2.setFill(Color.RED);
+        
+        typeArea.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String s2) {
+                i_am_typing();
+            }
+        });
     }
     
 }
