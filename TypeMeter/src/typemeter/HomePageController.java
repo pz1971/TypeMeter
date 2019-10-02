9fcb1aaf2d5f7e4030e834cbaec177bc3dfabcf1
Parvez Mia <parvez.pz1971@gmail.com>
2019-10-03 00:45:49 +0600

    timer added

--- TypeMeter/src/typemeter/HomePageController.java
+++ TypeMeter/src/typemeter/HomePageController.java
@@ -7,17 +7,25 @@ package typemeter;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -29,6 +37,11 @@ public class HomePageController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    
+    private static final int STARTTIME = 0;
+    private Timeline timeline;
+    private final IntegerProperty timeSeconds = new SimpleIntegerProperty(STARTTIME);
+    
     
     @FXML
     Text text1, text2, text3; // text1 = the portion already typed correctly, text2 = typed but not correct, text3 = not typed yet
@@ -45,8 +58,20 @@ public class HomePageController implements Initializable {
     @FXML
     ProgressBar pb;
     
+    @FXML
+    TextField speedField, accuracyField;
+    
+    @FXML
+    Label timerField, timerLabel ;
+    
     String script;
     int correct = 0, incorrect = 0;
+    
+    private void updateTime() {     // updates the time each seconds
+        // increment seconds
+        int seconds = timeSeconds.get();
+        timeSeconds.set(seconds+1);
+    }
     
     @FXML
     public void startTypingButtonActon(ActionEvent event){
@@ -61,6 +86,12 @@ public class HomePageController implements Initializable {
         correct = incorrect = 0;
         pb.setProgress(0.0) ;       // 0% has been typed
         typeArea.setText("");
+        
+        // reset the timer
+        timeline = new Timeline(new KeyFrame(Duration.seconds(1), evt -> updateTime())); 
+        timeline.setCycleCount(Animation.INDEFINITE); // repeat over and over again
+        timeSeconds.set(STARTTIME);
+        timeline.play();
     }
     
     private void endOfScript(){ // when user finishes typing the current script
@@ -74,6 +105,8 @@ public class HomePageController implements Initializable {
         
         pb.setProgress(1.0);
         correct = incorrect = 0;
+        
+        timeline.pause() ;
     }
     
     private void calculate(String typed){
@@ -95,7 +128,6 @@ public class HomePageController implements Initializable {
         
         if(typed.equals(script)){
             endOfScript();
-            return;
         }
         else{
             if(typed.length() > script.length()){
@@ -118,6 +150,8 @@ public class HomePageController implements Initializable {
         text1.setFill(Color.GREEN);
         text2.setFill(Color.RED);
         typeArea.setEditable(false) ;
+        
+        timerLabel.textProperty().bind(timeSeconds.asString());  //   for animation over time
         
         typeArea.textProperty().addListener(new ChangeListener<String>() {
             @Override
